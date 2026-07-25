b32a5af28d90108e28485f07d430e7d0f7cb7498
Jacob Sides <jacob.r.sides@gmail.com>
2026-07-25 14:31:03 -0400

    Rebuild portfolio in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	redirects: {
+		'/resume': {
+			status: 308,
+			destination: '/resume.pdf',
+		},
+		'/invite-bro': {
+			status: 302,
+			destination: 'https://youtu.be/dQw4w9WgXcQ?si=FIGWNgbGvOqVFwoG',
+		},
+	},
+});
